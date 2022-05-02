337caa0f498362b8c1064036dbb0a4a64138fa3b
Rizzardd <rizzarddirizu@gmail.com>
2022-05-02 10:07:48 -0300

    contador de numeros pares com enquanto usando js

---
+++ contador_pares.js
@@ -0,0 +1,15 @@
+const {
+  read, //lê texto literal, string
+  readInt, // lê números inteiros (números inteiros são números que não tem fração)
+  readBool, // lê valores lógicos
+  readFloat, // lê números fracionários ou reais (dentro dos números reais existem números inteiros, em ingles float)
+  write, // escreve um valor sem quebra de linha
+  writeline // escreve um valor com quebra de linha
+} = require('learning-console')
+
+let cont = readInt('escreva um numero:')
+
+while (cont < 20) {
+  cont += 2
+  writeline(cont)
+}
